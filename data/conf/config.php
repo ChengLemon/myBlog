3c304467b50b47d0a5e983346c3a66eb3fbc2f91
ChengLemon <da_cly@163.com>
2018-05-02 10:17:13 +0800

    update ignore

---
+++ data/conf/config.php
@@ -0,0 +1,4 @@
+<?php	return array (
+  'cmf_default_theme' => 'home',
+  'cmf_admin_default_theme' => 'admin',
+);
---
+++ data/conf/route.php
@@ -0,0 +1,118 @@
+<?php	return array (
+  'xinwen/:id' => 
+  array (
+    0 => 'portal/Article/index?cid=8',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'question/:id' => 
+  array (
+    0 => 'community/Article/index?cid=8',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'discuss/:id' => 
+  array (
+    0 => 'community/Article/index?cid=11',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'suggest/:id' => 
+  array (
+    0 => 'community/Article/index?cid=12',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'notice/:id' => 
+  array (
+    0 => 'community/Article/index?cid=13',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'station/:id' => 
+  array (
+    0 => 'community/Article/index?cid=14',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'onlyadmin/:id' => 
+  array (
+    0 => 'community/Article/index?cid=17',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'warmpass/:id' => 
+  array (
+    0 => 'community/Article/index?cid=15',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+      'cid' => '\d+',
+    ),
+  ),
+  'cheng1994$' => 'admin/Index/index',
+  'xinwen' => 
+  array (
+    0 => 'portal/List/index?id=8',
+    1 => 
+    array (
+    ),
+    2 => 
+    array (
+      'id' => '\d+',
+    ),
+  ),
+  'question' => 'community/List/index?id=8',
+  'discuss' => 'community/List/index?id=11',
+  'suggest' => 'community/List/index?id=12',
+  'notice' => 'community/List/index?id=13',
+  'station' => 'community/List/index?id=14',
+  'warmpass' => 'community/List/index?id=15',
+  'onlyadmin' => 'community/List/index?id=17',
+  'detail/:id' => 'community/Article/detail',
+);
